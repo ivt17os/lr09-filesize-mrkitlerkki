2365c28dc8cbe44e2b9c056a4d29b9ebb8b285a3
Kirill Kirillin <a@d.com>
2019-02-04 09:38:19 +0900

    done!

--- FolderSize/FolderSize.cpp
+++ FolderSize/FolderSize.cpp
@@ -23,15 +23,23 @@ void dfs() {
     hFind = FindFirstFile(L"*", &res);   // найти первый файл
  
     do {
-        count++; // некоторые файлы не считаются??
-        _tprintf(TEXT("file #%d is <%s>\n"), count, res.cFileName);
- 
-        // if (...) { // если это подпапка
-        // 	здесь будет обход в глубину
-        // }
-        // else {// это файл
-        // size+=res....
-        // }
+		if (wcscmp(res.cFileName, L".")==0 || 
+			wcscmp(res.cFileName, L"..")==0) 
+			continue;
+
+		_tprintf (TEXT("file #%d is <%s>\n"), count, res.cFileName);
+			
+	    if((res.dwFileAttributes & 16) == 16) { //если это подпапка
+        // здесь будет обход в глубину
+			SetCurrentDirectory(res.cFileName);
+			dfs();
+			SetCurrentDirectory(L"..");
+        }
+		else	
+		{ // это файл
+		size += res.nFileSizeLow;
+		count++; // некоторые файлы не считаются??
+        }
     } while (FindNextFile(hFind, &res) != 0);
     FindClose(hFind);
 }
